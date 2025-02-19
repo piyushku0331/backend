024d6e99b53da47ba70d2eb42ca8df3510a5c93d
piyushku0331 <159936773+piyushku0331@users.noreply.github.com>
2025-02-19 17:08:55 +0530

    my new commit for today

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express=require("express")
-const app=express()
-app.set("view engine","ejs")
-app.get("/",(req,res)=>{
-    // res.send("Server Created")
-    // res.sendFile("Views/index.ejs",{root:__dirname})
-    let brandName="Amazon.com"
-    res.render("index",{brandName:brandName})
+const express=require('express');
+const path=require('path');
+
+let app=express();
+app.listen(3000,function(){
+    console.log("server started at 3000");
 })
-app.listen(3000,()=>{
-    console.log("Server running on port 3000");
+app.use(express.urlencoded("true"));
+app.get("/",function(req,res){
+    let path=__dirname+"/register.html";
+    res.sendFile(path);
 })
